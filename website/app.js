5103d9a70f701ca12b580f4256c7fdc7def47fe7
jac21984 <jac21984@me.com>
2022-09-27 01:34:17 -0700

    added webpack 3

--- website/app.js
+++ website/app.js
@@ -1,5 +1,7 @@
 /* Global Variables */
 
+import "./style.scss";
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
