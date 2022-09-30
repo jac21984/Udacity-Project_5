f5582cdf6c8152ab98041fa284bee0a4bd24dc0d
jac21984 <jac21984@me.com>
2022-09-30 01:52:12 -0700

    added laoders

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,2 @@
 (()=>{"use strict";console.log((function(e){console.log("::: Running checkForName :::",e),["Picard","Janeway","Kirk","Archer","Georgiou"].includes(e)&&alert("Welcome, Captain!")})),alert("I EXIST")})();
+//# sourceMappingURL=main.js.map
