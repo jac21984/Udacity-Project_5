6b285fb66fb4e122fa261c0b9266b941274d5004
jac21984 <jac21984@me.com>
2022-09-30 02:20:46 -0700

    added mode feature

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,15 +1,11 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+// TODO: Require the html-webpack-plugin
 
 module.exports = {
     entry: './src/client/index.js',
-    output: {
-        libraryTarget: 'var',
-        library: 'Client'
-    },
-    mode: 'development',
+	mode: 'development',
     devtool: 'source-map',
     module: {
         rules: [
@@ -17,26 +13,15 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
+    // TODO: Add the plugin for index.html
     plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        }),
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        })
-    ]
+    new HtmlWebPackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+    })
+]
+    // Don't forget your commas!
 }
