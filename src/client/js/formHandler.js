93823efcb5fa53c4178f6064c93fb3d8c6fc5e6e
jac21984 <jac21984@me.com>
2022-10-19 02:16:42 -0700

    Update formHandler.js

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,15 +1,14 @@
 function handleSubmit(event) {
     event.preventDefault()
 	
-	let formText = document.getElementById('url').value
+	let urlText = document.getElementById('url').value
 	
-    if(Client.validateURL(formText)) {
+    if(Client.validateURL(urlText)) {
 
 		console.log('::: Form Submitted :::')
 		
-		postData('http://localhost:8081/api', {url: formText})
+		mcAPI('http://localhost:8081/api', {url: urlText})
 		.then(function(res) {
-			
 			document.getElementById("language").innerHTML = `Language: ${res.language_list[0].name}`;
 			var flagURL = 'https://countryflagsapi.com/svg/'+String(res.language_list[0].language);
 			document.getElementById("flag").src = flagURL;
@@ -19,8 +18,7 @@ function handleSubmit(event) {
 	}
 }
 
-const postData = async (url = "", data = {}) => {
-    console.log('Analyzing:', data);
+const mcAPI = async (url = "", data = {}) => {
     const response = await fetch(url, {
         method: 'POST',
         credentials: 'same-origin',
@@ -31,9 +29,9 @@ const postData = async (url = "", data = {}) => {
         body: JSON.stringify(data)
     });
     try {
-        const newData = await response.json();
-        console.log('Data received:', newData)
-        return newData;
+        const mcData = await response.json();
+        console.log('Returned Data:', mcData)
+        return mcData;
     } catch (error) {
         console.log('error', error);
     }
